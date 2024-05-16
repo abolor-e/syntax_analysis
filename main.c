d3fec20bc3dd7cc7e834c02dde21133efeb1768e
Anar-Erdene Bolor-Erdene <anarsmacbook@Anar-Erdenes-MacBook-Air.local>
2024-05-16 13:46:53 +0200

    shit

--- main.c
+++ main.c
@@ -10,6 +10,9 @@ int main(void)
     inp = readline("");
     input = ft_lexer(inp);
     parsing_table = ms_init_parsing_table();
-    printf("%d, %d, %d, %d\n", input->type, input->next->type, input->next->next->type, input->next->next->next->type);
+    printf("%d, %d, %d, %d, %d\n", input->type, input->next->type, input->next->next->type, input->next->next->next->type, input->next->next->next->next->type);
     tree = syntax_analysis(input, parsing_table);
+    if (!tree)
+        printf("hello\n");
+    printf("%d, %d, %d\n", tree->type, tree->left->type, tree->left->right->type);
 }
